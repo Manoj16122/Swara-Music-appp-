e8208d52339e87f334bb056a0b9a960367d8d7b0
Manoj16122 <87191205+Manoj16122@users.noreply.github.com>
2021-07-27 20:27:00 +0530

    Add files via upload

---
+++ files/admin_side_bar.php
@@ -0,0 +1,18 @@
+<div class="col-md-4 pl-0">
+	<div class="list-group">
+	  <a href="#" class="list-group-item list-group-item-action bg-light text-dark rounded-0">
+	    Admin Dashboard
+	  </a>
+	  <a href="admin_song_upload.php" class="list-group-item list-group-item-action">
+	    Upload Music
+	  </a>
+	  <a href="admin_songs.php" class="list-group-item list-group-item-action">My Music</a> 
+	  <a href="artist_add_new.php" class="list-group-item list-group-item-action">
+	    Add new Artist
+	  </a>
+	  <a href="admin_artist.php" class="list-group-item list-group-item-action rounded-0">
+	  All Artists</a>
+	  <a href="event.php" class="list-group-item list-group-item-action rounded-0">
+	  Events</a>
+	 </div>
+</div>
---
+++ files/footer.php
@@ -0,0 +1,19 @@
+ 	 <div class = "container">
+ 	 <footer class="border-top border-dark row mt-3 mt-md-5 pt-3">
+ 	<div class="col-md-3 text-center">
+ 		(c) SWARA - 2020
+ 	</div>
+ 	<div class="col-md-3 text-center text-dark">
+ 		<a href="#" title="" class="text-dark">facebook</a>
+ 	</div>
+ 	<div class="col-md-3 text-center text-dark">
+ 		<a href="#" title="" class="text-dark">Instagram</a>
+ 	</div>
+ 	<div class="col-md-3 text-center text-dark">
+ 		<a href="#" title="" class="text-dark">YouTube</a>
+ 	</div>
+    </footer>
+ </div>
+</div>
+</body>
+</html>
